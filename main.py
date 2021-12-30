a73263a66631eca66656e4dc3a50162e85239af6
brylimz <superuser@brylimz.com>
2021-12-30 15:37:17 +0700

    continue at home

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import requests
-
+MY_LAT = "-5.429750"
+MY_LNG = "105.262268"
+current = "current"
+minutely = "minutely"
+hourly = "hourly"
+daily = "daily"
+alerts = "alerts"
 api_key = "cde3aed6ead21fea067c6db611474f24"
 
+
+weather = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LAT}&lon={MY_LNG}&exclude={alerts}&appid={api_key}")
+weather.raise_for_status()
+json_data = weather.json()
+print(json_data)
