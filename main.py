9cfae22a6369ba8a4de63588162fcdc7a30c32ee
brylimz <superuser@brylimz.com>
2021-12-30 15:49:44 +0700

    continue at home

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ minutely = "minutely"
 hourly = "hourly"
 daily = "daily"
 alerts = "alerts"
+
 api_key = "cde3aed6ead21fea067c6db611474f24"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+weather_params = {
+    "lat": MY_LAT,
+    "lon": MY_LNG,
+    "appid": api_key,
+}
 
-
-weather = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LAT}&lon={MY_LNG}&exclude={alerts}&appid={api_key}")
-weather.raise_for_status()
-json_data = weather.json()
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+json_data = response.json()
 print(json_data)
